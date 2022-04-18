1e01f050557b5975d8ea99c00c1d01ed5be152eb
Saga- <danielorangerl@gmail.com>
2022-04-18 12:16:35 +0100

    3.3

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  person ? response.json(person) : response.status(404).send('Person not found');
+});
+
+
 app.get('/info', (request, response) => {
   const size = persons.length;
   response.send(`

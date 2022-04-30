80aadd29acbd7cb383ebf490f6855374639226cd
Saga- <danielorangerl@gmail.com>
2022-04-30 12:25:17 +0100

    3.10

--- frontend/src/services/personApiService.js
+++ frontend/src/services/personApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://immense-atoll-49825.herokuapp.com/api/persons'
 
 const getAllPersons = () => {
   const request = axios.get(baseUrl);
